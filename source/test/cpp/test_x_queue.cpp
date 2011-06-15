7d3c08d3a6c0e30f5af1d2de0c452939eee08a5c
Jurgen Kluft <jurgen@virtuos.com.cn>
2011-06-15 18:05:15 +0800

    +: added unittest for queue

---
+++ source/test/cpp/test_x_queue.cpp
@@ -0,0 +1,153 @@
+#include "xbase\x_types.h"
+#include "xunittest\xunittest.h"
+
+#include "xatomic\x_queue.h"
+
+UNITTEST_SUITE_BEGIN(queue)
+{
+    UNITTEST_FIXTURE(main)
+    {
+        UNITTEST_FIXTURE_SETUP() {}
+        UNITTEST_FIXTURE_TEARDOWN() {}
+
+		UNITTEST_TEST(construct1)
+		{
+			xcore::atomic::queue<xcore::s32> f;
+			f.init(1);
+			CHECK_TRUE(f.valid());
+
+			CHECK_EQUAL(true, f.empty());
+			CHECK_EQUAL(1, f.max_size());
+			CHECK_EQUAL(1, f.room());
+		}
+
+		UNITTEST_TEST(construct2)
+		{
+			xcore::atomic::queue<xcore::s32> f;
+			f.init(16);
+			CHECK_TRUE(f.valid());
+
+			CHECK_EQUAL(true, f.empty());
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+		}
+		
+		UNITTEST_TEST(push_begin)
+		{
+			xcore::atomic::queue<xcore::s32> f;
+			f.init(16);
+			CHECK_TRUE(f.valid());
+
+			CHECK_EQUAL(true, f.empty());
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+
+			xcore::s32* i1 = f.push_begin();
+			CHECK_NOT_NULL(i1);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+
+			xcore::s32* i2 = f.push_begin();
+			CHECK_NOT_NULL(i2);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+		}
+
+		UNITTEST_TEST(push_cancel)
+		{
+			xcore::atomic::queue<xcore::s32> f;
+			f.init(16);
+			CHECK_TRUE(f.valid());
+
+			CHECK_EQUAL(true, f.empty());
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+
+			xcore::s32* i1 = f.push_begin();
+			CHECK_NOT_NULL(i1);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+			f.push_cancel(i1);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+
+			xcore::s32* i2 = f.push_begin();
+			CHECK_NOT_NULL(i2);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+			f.push_cancel(i2);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+		}
+
+		UNITTEST_TEST(push_commit)
+		{
+			xcore::atomic::queue<xcore::s32> f;
+			f.init(16);
+			CHECK_TRUE(f.valid());
+
+			CHECK_EQUAL(true, f.empty());
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+
+			xcore::s32* i1 = f.push_begin();
+			CHECK_NOT_NULL(i1);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+			f.push_commit(i1);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(15, f.room());
+
+			xcore::s32* i2 = f.push_begin();
+			CHECK_NOT_NULL(i2);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(15, f.room());
+			f.push_commit(i2);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(14, f.room());
+
+			f.clear();
+			CHECK_FALSE(f.valid());
+		}
+
+		UNITTEST_TEST(push)
+		{
+			xcore::atomic::queue<xcore::s32> f;
+			f.init(16);
+			CHECK_TRUE(f.valid());
+
+			CHECK_EQUAL(true, f.empty());
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(16, f.room());
+
+			f.push(55);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(15, f.room());
+			f.push(77);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(14, f.room());
+
+			f.push(88);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(13, f.room());
+			f.push(99);
+			CHECK_EQUAL(16, f.max_size());
+			CHECK_EQUAL(12, f.room());
+
+			xcore::s32 i;
+			f.pop(i);
+			CHECK_EQUAL(55, i);
+			f.pop(i);
+			CHECK_EQUAL(77, i);
+			f.pop(i);
+			CHECK_EQUAL(88, i);
+			f.pop(i);
+			CHECK_EQUAL(99, i);
+
+			f.clear();
+			CHECK_FALSE(f.valid());
+		}
+
+	}
+}
+UNITTEST_SUITE_END
